78e5b3d423038369df6314d71bc08ecca2e08f95
Mikhail Burshteyn <mdburshteyn@gmail.com>
2020-03-01 00:49:18 +0300

    Add ability for plugins to parse and use config

--- flake8_plugin_utils/__init__.py
+++ flake8_plugin_utils/__init__.py
@@ -1,2 +1,11 @@
-from .plugin import Error, Plugin, Visitor  # noqa:F401
-from .utils import assert_error, assert_not_error, is_none  # noqa:F401
+from .plugin import Error, Plugin, Visitor
+from .utils import assert_error, assert_not_error, is_none
+
+__all__ = (
+    'Error',
+    'Plugin',
+    'Visitor',
+    'assert_error',
+    'assert_not_error',
+    'is_none',
+)
